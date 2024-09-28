5a9fe92a1e0e593430eb293d9cb84350f02d6b53
Dhemas Nurjaya <103254895+dhemasnurjaya@users.noreply.github.com>
2024-09-29 01:15:17 +0700

    replace envied with dart native env (#15)

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,25 +1,18 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
 abstract class Env {
   String get authPublicKey;
+
   String get sentryDsn;
 }
 
-@Envied(path: '.env')
 class EnvImpl implements Env {
-  @EnviedField(varName: 'AUTH_PUBLIC_KEY', obfuscate: true)
-  static final String _authPublicKey = _EnvImpl._authPublicKey;
-
-  @EnviedField(varName: 'SENTRY_DSN', defaultValue: '', obfuscate: true)
-  static final String _sentryDsn = _EnvImpl._sentryDsn;
+  @override
+  String get authPublicKey => const String.fromEnvironment('AUTH_PUBLIC_KEY');
 
   @override
-  String get authPublicKey => _authPublicKey;
-
-  @override
-  String get sentryDsn => _sentryDsn;
+  String get sentryDsn => const String.fromEnvironment(
+        'SENTRY_DSN',
+        defaultValue: '',
+      );
 }
