a5d2461e35d7c46b694dbe2e4c1763d2ff61138a
Dhemas Nurjaya <103254895+dhemasnurjaya@users.noreply.github.com>
2024-10-23 13:17:34 +0700

    change donation icon

--- lib/features/quote/presentation/quote_page.dart
+++ lib/features/quote/presentation/quote_page.dart
@@ -76,7 +76,7 @@ class _QuotePageState extends State<QuotePage> {
                 onPressed: () {
                   context.router.push(const DonationRoute());
                 },
-                icon: const FaIcon(FontAwesomeIcons.mugHot),
+                icon: const FaIcon(FontAwesomeIcons.circleDollarToSlot),
               ),
               IconButton(
                 onPressed: () {
